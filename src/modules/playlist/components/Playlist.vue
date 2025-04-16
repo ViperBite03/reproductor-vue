6510926c02ab3fc49697d02091f732be180a68b2
ViperBite03 <layitamartin@gmail.com>
2025-04-16 14:28:12 +0200

    playliststststs

--- src/modules/playlist/components/Playlist.vue
+++ src/modules/playlist/components/Playlist.vue
@@ -1,14 +1,31 @@
-<script setup></script>
+<script setup>
+  import PlaylistItem from '@/modules/playlist/components/PlaylistItem.vue'
+</script>
 
 <script setup></script>
 
 <style lang="scss">
   #playlist {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .p-list {
+      display: flex;
+      gap: 15px;
+      height: 100%;
+    }
   }
 </style>
 
 <template>
   <div id="playlist">
     <h2 class="g-title">PLAYLIST</h2>
+
+    <div class="p-list">
+      <div class="p-item" v-for="n in 4" :key="n">
+        <PlaylistItem> </PlaylistItem>
+      </div>
+    </div>
   </div>
 </template>
--- src/modules/songlist/components/Songlist.vue
+++ src/modules/songlist/components/Songlist.vue
@@ -1,5 +1,6 @@
 <script setup>
-  import AddSong from '@/modules/songlist/components/AddSong.vue';
+  import AddSong from '@/modules/songlist/components/AddSong.vue'
+  import Song from '@/modules/songlist/components/Song.vue'
 </script>
 
 <style lang="scss">
@@ -8,9 +9,11 @@
 </style>
 
 <template>
-  <div id="songlist">
-    <h2 class="g-title">SONGLIST</h2>
+  <div id="settings">
+    <h2 class="g-title">SongList</h2>
 
-    <AddSong />
+    <div class="queue-list" v-for="n in 6" :key="n">
+      <Song> </Song>
+    </div>
   </div>
 </template>
